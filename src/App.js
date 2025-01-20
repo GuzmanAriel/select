34f5eacd886bb818359e3896c7514ca3570f6bb1
GuzmanAriel <guzmanariel525@gmail.com>
2025-01-20 11:47:38 -0600

    organized files and created more CSS for the new tournament cards

--- src/App.js
+++ src/App.js
@@ -4,63 +4,11 @@ import Dashboard from './pages/DashboardPage';
 import {Route, Routes, Navigate} from "react-router-dom";
 import Header from './components/Header';
 import Signup from './pages/SignUpPage';
-
-
-const TOURNAMENT_LIST = [
-  {
-    id: 0,
-    name: "Jason's Birthday Tournament",
-    date: "5/17/2025",
-    start_time: "9:00am",
-    location: "Zilker Park",
-    tournament_type: "BYO4",
-    playoff_elimination_type: "Single",
-    playoff_bracket_number: 2,
-    prizes: {
-      first_place: "$1000 and gift card",
-      second_place: "$500 and gift card",
-      third_place: "$100"
-    },
-    additional_notes: "We will be in the far side of Zilker park, parking is available in the lot across the park or accross the street"
-  },
-  {
-    id: 1,
-    name: "First Tournament",
-    date: "4/20/2025",
-    start_time: "9:00am",
-    location: "Zilker Park - first",
-    tournament_type: "BYO3",
-    playoff_elimination_type: "Single",
-    playoff_bracket_number: 1,
-    prizes: {
-      first_place: "$1000 and gift card",
-      second_place: "$500 and gift card",
-      third_place: "$100"
-    },
-    additional_notes: "We will be in the far side of Zilker park, parking is available in the lot across the park or accross the street"
-  },
-  {
-    id: 2,
-    name: "End of Year Tournament",
-    date: "11/18/2024",
-    start_time: "9:00am",
-    location: "Zilker Park",
-    tournament_type: "BYO4",
-    playoff_elimination_type: "Single",
-    playoff_bracket_number: 1,
-    prizes: {
-      first_place: "$1000 and gift card",
-      second_place: "$500 and gift card",
-      third_place: "$100"
-    },
-    additional_notes: "We will be in the far side of Zilker park, parking is available in the lot across the park or accross the street"
-  }
-]
-
+import { selectAllTournaments} from './utils/tournaments/tournamentLists';
 
 function App() {
-  const [tournamentList, setTournamentList] = useState(TOURNAMENT_LIST);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const tournamentList  = selectAllTournaments();
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <div>
       <header className="border-secondary border-bottom">
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,9 +1,119 @@
-const Dashboard = () => {
+import {
+    Nav,
+    NavItem,
+    NavLink,
+    TabContent,
+    TabPane,
+    Row,
+    Col,
+    Card,
+    CardTitle,
+    CardText,
+    Button,
+  } from 'reactstrap';
+  import { useState } from 'react';
+  import { selectAllTournaments} from '../utils/tournaments/tournamentLists';
+  
+  const Dashboard = (props) => {
+    // State to track the active tab
+    const [activeTab, setActiveTab] = useState("1");
+    const tournaments = selectAllTournaments();
+  
+    // Function to toggle the active tab
+    const toggleTab = (tab) => {
+      if (activeTab !== tab) {
+        setActiveTab(tab);
+      }
+    };
+  
     return (
-        <>
-        <h1>Dashboard</h1>
-        </>
-    )
-}
-
-export default Dashboard;
+      <div id="dashboard" className="ts-dashboard">
+        <Nav tabs className="border-0">
+          <NavItem>
+            <NavLink
+              className={`${activeTab === "1" ? "active" : ""} `}
+              onClick={() => toggleTab("1")}
+            >
+              Current
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={activeTab === "2" ? "active" : ""}
+              onClick={() => toggleTab("2")}
+            >
+              Upcoming
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={activeTab === "3" ? "active" : ""}
+              onClick={() => toggleTab("3")}
+            >
+              Past
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={activeTab} className="mt-5">
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 1 Contents</h4>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col sm="6">
+                <Card body>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>
+                    With supporting text below as a natural lead-in to additional
+                    content.
+                  </CardText>
+                  <Button>Go somewhere</Button>
+                </Card>
+              </Col>
+              <Col sm="6">
+                <Card>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>
+                    With supporting text below as a natural lead-in to additional
+                    content.
+                  </CardText>
+                  <Button>Go somewhere</Button>
+                </Card>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="3">
+            <Row>
+              <Col sm="6">
+                <Card body>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>
+                    With supporting text below as a natural lead-in to additional
+                    content.
+                  </CardText>
+                  <Button>Go somewhere</Button>
+                </Card>
+              </Col>
+              <Col sm="6">
+                <Card>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>
+                    With supporting text below as a natural lead-in to additional
+                    content.
+                  </CardText>
+                  <Button>Go somewhere</Button>
+                </Card>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
+      </div>
+    );
+  };
+  
+  export default Dashboard;
+  
