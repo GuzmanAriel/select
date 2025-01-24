ee1dfe6e1956e5f65751bc5ef78fa4386a8aa928
GuzmanAriel <guzmanariel525@gmail.com>
2025-01-23 19:16:01 -0600

    Started on the card components

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -7,6 +7,7 @@ import {
     Row,
     Col,
     Card,
+    CardBody,
     CardTitle,
     CardText,
     Button,
@@ -57,8 +58,32 @@ import {
         <TabContent activeTab={activeTab} className="mt-5">
           <TabPane tabId="1">
             <Row>
-              <Col sm="12">
-                <h4>Tab 1 Contents</h4>
+              <Col sm="6">
+                <Card className="bg-transparent border-1 border-white">
+                <img
+                    alt="Sample"
+                    src="https://picsum.photos/200/80"
+                  />
+                  <CardBody>
+                    <CardTitle className="fw-bold card-title">First of Season Tournament</CardTitle>
+                    <CardText>
+                      With supporting text below as a natural lead-in to additional
+                      content.
+                    </CardText>
+                    <Button>Go somewhere</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+
+              <Col sm="6">
+                <Card>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>
+                    With supporting text below as a natural lead-in to additional
+                    content.
+                  </CardText>
+                  <Button>Go somewhere</Button>
+                </Card>
               </Col>
             </Row>
           </TabPane>
